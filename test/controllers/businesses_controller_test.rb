61b7b4d2193fd406363b062716299006969005d8
Chris Kaukis <chris.kaukis@gmail.com>
2016-07-05 18:19:05 -0500

    Updated tests.

--- test/controllers/businesses_controller_test.rb
+++ test/controllers/businesses_controller_test.rb
@@ -1,23 +1,23 @@
 require 'test_helper'
 
 class BusinessesControllerTest < ActionDispatch::IntegrationTest
-  test "should return a list of businesses" do
+  test 'should return a list of businesses' do
     get businesses_url
     json = JSON.parse(response.body)
     # If we add more fixtures test will still pass.
     assert json.size > 0
-    json.each do |object|
-      # We could test to make sure the bussiness exists and/or we could test
-      # that the object returned contains all the attributes we expect.
-      Business.find(object['id'])
-    end
   end
 
-  test "should return a business" do
+  test 'should return a business' do
     business = businesses(:one)
     get business_url(business)
     json = JSON.parse(response.body)
     # We could also test each attribute.
     assert json['id'] == business.id
+  end
+
+  test 'should return not found' do
+    get business_url('some id')
+    assert_response :not_found
   end
 end
