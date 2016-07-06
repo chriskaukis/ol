6bd6a06a289ed17a3812a47d1ca53a58d85356a1
Chris Kaukis <chris.kaukis@gmail.com>
2016-07-06 00:07:21 -0500

    Default format.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :businesses, only: [:index, :show]
+  resources :businesses, only: [:index, :show], defaults: { format: :json }
 
   match '*path', to: 'application#raise_not_found', via: :any
 end
