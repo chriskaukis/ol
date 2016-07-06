46234d904a7ca21bff902423c8d4ca8ac52bd4e9
Chris Kaukis <chris.kaukis@gmail.com>
2016-07-06 00:09:02 -0500

    Updated tests and controller methods.

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,11 +1,10 @@
 class BusinessesController < ApplicationController
   def index
-    businesses = Business.order(:id).page(params[:page]).per(50)
-    render json: businesses
+    @businesses = Business.order(:id).page(params[:page]).per(50)
+    set_link_headers(@businesses)
   end
 
   def show
-    business = Business.find(params[:id])
-    render json: business
+    @business = Business.find(params[:id])
   end
 end
--- test/controllers/businesses_controller_test.rb
+++ test/controllers/businesses_controller_test.rb
@@ -2,22 +2,26 @@ require 'test_helper'
 
 class BusinessesControllerTest < ActionDispatch::IntegrationTest
   test 'should return a list of businesses' do
-    get businesses_url
+    get businesses_url, as: :json
     json = JSON.parse(response.body)
     # If we add more fixtures test will still pass.
     assert json.size > 0
   end
 
+  test 'should return link headers' do
+    skip 'We either need more test data or a way to override per page or another means where the Link header will get set.'
+  end
+
   test 'should return a business' do
     business = businesses(:one)
-    get business_url(business)
+    get business_url(business), as: :json
     json = JSON.parse(response.body)
     # We could also test each attribute.
     assert json['id'] == business.id
   end
 
   test 'should return not found' do
-    get business_url('some id')
+    get business_url('some id'), as: :json
     assert_response :not_found
   end
 end
