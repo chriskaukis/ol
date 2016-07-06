ca697566951925a5434bd4b2e90186ed24e02f30
Chris Kaukis <chris.kaukis@gmail.com>
2016-07-06 00:08:37 -0500

    Added Link headers method for paginated collections.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,18 @@ class ApplicationController < ActionController::API
     def not_found(exception)
       render status: 404, json: exception
     end
+
+    def set_link_headers(paged)
+      links = {}
+      links[:first] = 1 if paged.total_pages > 1 && !paged.first_page?
+      links[:last] = paged.total_pages if paged.total_pages > 1 && !paged.last_page?
+      links[:next] = paged.next_page if !paged.last_page? && !paged.out_of_range?
+      links[:prev] = paged.prev_page if !paged.first_page? && !paged.out_of_range?
+
+      link_headers = []
+      links.each do |key, value|
+        link_headers << %Q{<#{url_for(request.query_parameters.merge({ page: value }))}>; rel="#{key}"}
+      end
+      headers['Link'] = link_headers.join(', ')
+    end
 end
